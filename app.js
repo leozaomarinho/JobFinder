bd54ac92215fa5f4eab0d636306c198461000ca0
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2022-11-22 15:02:01 -0300

    update index

--- app.js
+++ app.js
@@ -2,14 +2,23 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection.js');
+const bodyParser = require('body-parser');
 
+/*
+router.get('/test',(req,res) =>{
+    res.send('deu certo!')
+})*/
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`O express esta rodando na porta ${PORT}!`)
 })
 
-//dbc onnection
+//body-parser
+app.use(bodyParser.urlencoded({extended: false }))
+
+
+//db  connection
 db
     .authenticate()
     .then(() => {
@@ -23,3 +32,6 @@ db
 app.get('/', (req, res) => {
     res.send('Esta funcionando com nodemon');
 });
+
+//job routes
+app.use('/jobs',require('./routes/jobs'));
